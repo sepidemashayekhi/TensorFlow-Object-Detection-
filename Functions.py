72f7344e97922dfd34dc07d2b21b741bd7c0bf97
Sepide Mashayekhi <sepidehmashayekhi7@gmail.com>
2022-01-30 19:36:42 +0330

    Final Commit_3

--- Functions.py
+++ Functions.py
@@ -18,13 +18,15 @@ class Detect:
         self.modelName=filename[:filename.index('.')]
         self.cachDir="./pretrained_models"
         os.makedirs(self.cachDir,exist_ok=True)
-        # get_file(fname=filename,origin=modelURL,
-        #          cache_dir=self.cachDir,cache_subdir='checkpoints',extract=True)
+        get_file(fname=filename,origin=modelURL,
+                 cache_dir=self.cachDir,cache_subdir='checkpoints',extract=True)
+
     def loadModel(self):
         print ('Loadding Model'+self.modelName)
         tf.keras.backend.clear_session()
         self.model=tf.saved_model.load(os.path.join(self.cachDir,"checkpoints", self.modelName,'saved_model'))
         print("Model"+self.modelName+"Load is finish")
+
     def creatbondingBox(self,image,threshold=0.5):
         inputTensor=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
         inputTensor=tf.convert_to_tensor(inputTensor)
@@ -43,7 +45,7 @@ class Detect:
                 bbox=tuple(bboxs[i].tolist())
                 classConfidence=round(100*classeScores[i])
                 classIndex=classIndexes[i]
-                classLabelText=self.classList[classIndex]
+                classLabelText=self.classList[classIndex-1]
                 classColor=self.ColorList[classIndex]
                 displayText='{} :{} %'.format(classLabelText,classConfidence)
                 ymin,xmin,ymax,xmax=bbox
@@ -52,25 +54,26 @@ class Detect:
                 cv2.rectangle(image,(xmin,ymin),(xmax,ymax),color=classColor,thickness=1)
                 cv2.putText(image,displayText,(xmin,ymin-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,classColor,2)
 
-                # lineWidth=min(int((xmax-xmin)*0.2),int((ymax-ymin)*0.2))
-                #
-                # cv2.line(image,(xmin,ymin),(xmin+lineWidth,ymax),color=classColor,thickness=5)
-                # cv2.line(image, (xmin, ymin), (xmin , ymin+lineWidth), color=classColor, thickness=5)
-                #
-                # cv2.line(image, (xmin, ymin), (xmax - lineWidth, ymax), color=classColor, thickness=5)
-                # cv2.line(image, (xmin, ymin), (xmax, ymin+ lineWidth), color=classColor, thickness=5)
+                lineWidth=min(int((xmax-xmin)*0.2),int((ymax-ymin)*0.2))
+
+                cv2.line(image,(xmin,ymin),(xmin+lineWidth,ymin),color=classColor,thickness=5)
+                cv2.line(image, (xmin, ymin), (xmin , ymin+lineWidth), color=classColor, thickness=5)
+
+                cv2.line(image, (xmax, ymin), (xmax - lineWidth, ymin), color=classColor, thickness=5)
+                cv2.line(image, (xmax, ymin), (xmax, ymin+ lineWidth), color=classColor, thickness=5)
+                #################################
+                cv2.line(image, (xmin, ymax), (xmin + lineWidth, ymax), color=classColor, thickness=5)
+                cv2.line(image, (xmin, ymax), (xmin, ymax -lineWidth), color=classColor, thickness=5)
+
+                cv2.line(image, (xmax, ymax), (xmax - lineWidth, ymax), color=classColor, thickness=5)
+                cv2.line(image, (xmax, ymax), (xmax, ymax - lineWidth), color=classColor, thickness=5)
 
 
             return  image
-
-
-
     def predict_image(self,imagePath,threshold=0.5):
         image=cv2.imread(imagePath)
         bboxImage=self.creatbondingBox(image,threshold)
-
-
-
+        cv2.namedWindow('result',cv2.WINDOW_NORMAL)
         cv2.imshow("result",bboxImage)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
--- Run_codes.py
+++ Run_codes.py
@@ -1,10 +1,32 @@
+import time
+
 from Functions import Detect
-modelURL='http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz'
+import cv2
+# modelURL='http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz'
+modelURL='http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d4_coco17_tpu-32.tar.gz'
+# modelURL='http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz'
 classifilepath='coco.names'
 threshold=0.5
 detector=Detect()
 detector.readClasses(classifilepath)
 detector.downloadModel(modelURL)
 detector.loadModel()
-imagePath='images/test/1.jpg'
+imagePath='images/test/2.jpg'
 detector.predict_image(imagePath,threshold)
+
+# real Time
+cam=cv2.VideoCapture(0)
+startTime=0
+while True:
+    nowTime=time.time()
+    fsp=1/(nowTime-startTime)
+    success,frame=cam.read()
+    Imagebbox=detector.creatbondingBox(frame,threshold=0.5)
+    cv2.putText(Imagebbox,"FPS"+str(int(fsp)),(20,70),cv2.FONT_HERSHEY_SIMPLEX,
+                1,color=(0,0,255),thickness=2)
+
+    cv2.imshow("frame",Imagebbox)
+    if cv2.waitKey(1)==27:
+        break
+
+
